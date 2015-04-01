1402379801217a4510d3b6b1bac6c3a5de7ee4d3
Marcos Essindi <me@12auto.de>
2015-04-01 12:41:49 +0200

    Not requiring railtie.rb

--- lib/railroady.rb
+++ lib/railroady.rb
@@ -1,5 +1,1 @@
 ['version', 'options_struct', 'models_diagram', 'controllers_diagram', 'aasm_diagram'].each { |f| require "railroady/#{f}" }
-
-module RailRoady
-  require 'railroady/railtie' if defined?(Rails)
-end
